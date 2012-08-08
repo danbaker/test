b7e3ab161930f7d24b7014ab2610073aeae5cd36
Dan Baker <dan.baker@domo.com>
2012-08-07 18:52:42 -0600

    Change number fourteen

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('The UtahJS World is AMAZING!');
+    response.send('The UtahJS World is AMAZING -- Hello planet');
 });
 
 var port = process.env.PORT || 5000;
