4b25da0624e1b00eb96eeb234d8114e8b88f7022
Dan Baker <dan.baker@domo.com>
2012-08-07 19:33:09 -0600

    added pg, but not working

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pg = require('pg');
 
 var app = express.createServer(express.logger());
 
@@ -10,3 +11,15 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
+
+
+
+
+// DEBUG DATABASE
+//pg.connect(process.env.DATABASE_URL, function(err, client) {
+//    var query = client.query('SELECT * FROM your_table');
+//
+//    query.on('row', function(row) {
+//        console.log(JSON.stringify(row));
+//    });
+//});
