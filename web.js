42340a4894b342728c75bce965f0c7df8f7aa50b
Kip Lawrence <klawrence@corda.com>
2012-08-07 19:07:54 -0600

    one

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('The UtahJS World is AMAZING -- Hello planet');
+    response.send('The UtahJS World is AMAZING -- Hello planet -- 1');
 });
 
 var port = process.env.PORT || 5000;
