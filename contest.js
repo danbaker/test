a26bea18242a43b5f716a845e0b5afcbd6f1ea90
Dan Baker <dan.baker@domo.com>
2012-09-14 16:02:14 -0600

    sandbox

--- contest.js
+++ contest.js
@@ -3,9 +3,17 @@
     CONTEST
 
 */
-
+var Sandbox = require('sandbox');
 
 // run a contest between P1 and P2 (call callback fnc when done)
 exports.runContest = function(id_p1, id_p2, fnc) {
-    fnc({note:"Not implemented yet"});
+    var s = new Sandbox();
+    var js = "var x = 5; console.log(x * x); x";
+    s.run( js, function( output ) {
+        // output.result = returned value
+        // output.console = returned console logs
+        fnc(output);
+//        console.log( "Example 10: " + output.console + "\n" )
+    });
+//    fnc({note:"Not implemented yet"});
 };
