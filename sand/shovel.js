eeae91606729c5b24959c41c65fbed1da59dd1ac
Dan Baker <dan.baker@domo.com>
2012-09-14 17:23:16 -0600

    sandbox

--- sand/shovel.js
+++ sand/shovel.js
@@ -30,7 +30,7 @@ var apiQWE = require('./api');          // API back to the contest
 function getSafeRunner() {
   var global = this;
   var api = apiQWE;          // API back to the contest
-  var test5 = api.get5();
+//  var test5 = api.get5();
   // Keep it outside of strict mode
   function UserScript(str) {
     // We want a global scoped function that has implicit returns.
