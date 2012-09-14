92c4be4c05fd966235d9ff2042561b413ca57fa1
Dan Baker <dan.baker@domo.com>
2012-09-14 17:24:39 -0600

    sandbox

--- sand/shovel.js
+++ sand/shovel.js
@@ -25,11 +25,11 @@ stdin.on( 'data', function( data ) {
 });
 stdin.on( 'end', run );
 
-var apiQWE = require('./api');          // API back to the contest
+//var apiQWE = require('./api');          // API back to the contest
 
 function getSafeRunner() {
   var global = this;
-  var api = apiQWE;          // API back to the contest
+//  var api = apiQWE;          // API back to the contest
 //  var test5 = api.get5();
   // Keep it outside of strict mode
   function UserScript(str) {
