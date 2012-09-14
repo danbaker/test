16ad9c969ca61aabb7455eb9ac04c0a1968faf0e
Dan Baker <dan.baker@domo.com>
2012-09-14 16:19:34 -0600

    sandbox

--- contest.js
+++ contest.js
@@ -3,7 +3,7 @@
     CONTEST
 
 */
-var Sandbox = require('sandbox');
+var Sandbox = require('./sand/sandbox');
 
 // run a contest between P1 and P2 (call callback fnc when done)
 exports.runContest = function(id_p1, id_p2, fnc) {
@@ -11,7 +11,7 @@ exports.runContest = function(id_p1, id_p2, fnc) {
     var js = "var x = 5; console.log(x * x); x";
     s.run( js, function( output ) {
         // output.result = returned value
-        // output.console = returned console logs
+        // output.console = returned console logs (doesn't seem to work on heroku)
         fnc(output);
 //        console.log( "Example 10: " + output.console + "\n" )
     });
