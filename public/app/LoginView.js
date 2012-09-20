a9aa51ace3986aa0b108ce4d6d4b6e6c6e1d6203
Kip Lawrence <klawrence@corda.com>
2012-09-19 18:43:04 -0600

    Added simple UI to register and log in.

--- public/app/LoginView.js
+++ public/app/LoginView.js
@@ -18,22 +18,30 @@ define([
 
         render: function() {
 
-            this.$el.html('<a href="#" class="navbar-link sign-in">Sign In</a>');
+            this.$el.html('<a href="#" class="navbar-link sign-in">Sign In</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="#" class="navbar-link register">Register</a>');
             return this;
         },
 
         _login: function() {
 
-            var button = '<div class="dropdown">' +
-                '<a  data-toggle="dropdown" href="#"><i class="icon-user icon-white"></i> Dan Baker</a>' +
-                '<ul class="dropdown-menu">' +
-                '<li><a href="#">Profile</a></li>' +
-                '<li><a href="#">Recent Games</a></li>' +
-                '<li class="divider"></li>' +
-                '<li><a href="#">Sign Out</a></li>' +
-                '</ul>' +
-                '</div>';
-            this.$el.html(button);
+//            var button = '<div class="dropdown">' +
+//                '<a  data-toggle="dropdown" href="#"><i class="icon-user icon-white"></i> Dan Baker</a>' +
+//                '<ul class="dropdown-menu">' +
+//                '<li><a href="#">Profile</a></li>' +
+//                '<li><a href="#">Recent Games</a></li>' +
+//                '<li class="divider"></li>' +
+//                '<li><a href="#">Sign Out</a></li>' +
+//                '</ul>' +
+//                '</div>';
+//            button = '<form class="form-inline">' +
+//                '<input type="text" class="input-small" placeholder="Email">' +
+//                '<input type="password" class="input-small" placeholder="Password">' +
+//                '<label class="checkbox">' +
+//                '<input type="checkbox"> Remember me' +
+//                '</label>' +
+//                '<button type="submit" class="btn">Sign in</button>' +
+//                '</form>';
+//            this.$el.html(button);
         }
 
     });
