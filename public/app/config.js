8eab3469d87be1fe4689a69c36a20c118dc7c122
Kip Lawrence <klawrence@corda.com>
2012-09-24 09:24:27 -0600

    Fix requirejs underscore config.

--- public/app/config.js
+++ public/app/config.js
@@ -17,6 +17,10 @@ require.config({
 
     shim:{
 
+        underscore:{
+            exports:"_"
+        },
+
         backbone:{
             deps:["jquery", "underscore"],
             exports:"Backbone"
