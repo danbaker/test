596d1ca00fd489a248c1ca623078f9712352a08d
Dan Baker <dan.baker@domo.com>
2012-10-04 17:54:48 -0600

    cap logs saved

--- sand2/shovel.js
+++ sand2/shovel.js
@@ -7,7 +7,7 @@ var lastJson = {};
 var consoleX = console;         // NOTE: this will send data BACK to server (do NOT use it)
 var processX = process;
 require('./log').setPrefix("CHILD");
-require('./logdb').setMaxLogs(4);
+require('./logdb').setMaxLogs(100);         // only record first 100 logs per external bot code
 var logX = require('./log').log;
 var util = require( 'util' );
 var path = require('path');
@@ -98,84 +98,3 @@ var waiting = function() {
         }, 5000);
     }
 };
-
-//var run2 = function() {
-//    trace("Inside of run2 ... sending packet back");
-//    packet.sendString("## 5 {{a:1}", process.stdout);
-////    waiting();
-//};
-//run2();
-
-
-
-//function getSafeRunner() {
-//    var global = this;
-//    // Keep it outside of strict mode
-//    function UserScript(str) {
-//        // We want a global scoped function that has implicit returns.
-//        return Function('return eval('+JSON.stringify(str+'')+')');
-//    }
-//    // place with a closure that is not exposed thanks to strict mode
-//    return function run(commx, src) {
-//        // stop argument / caller attacks
-//        "use strict";
-//        var send = function send(event) {
-//            "use strict";
-//            //
-//            // All comm must be serialized properly to avoid attacks, JSON or XJSON
-//            //
-//            commx.send(event, JSON.stringify([].slice.call(arguments,1)));
-//        };
-//        global.print = send.bind(global, 'stdout');
-//        global.console = {};
-//        global.console.log = send.bind(global, 'stdout');
-//        var result = UserScript(src)();
-//        send('end', result);
-//        // @TODO: allow js to continue running ???
-//        setTimeout( function() {
-//            send('end', result);
-//        }, 3000 );
-//    }
-//}
-
-// Run code
-//function run() {
-//    var context = Script.createContext();
-//    // alter the context of the script ... add contest-api to it
-//    context.contestAPI = api;
-//    context.contestAPI.debugLog = function(msg) { consoleA.push(msg); };
-//
-//    var safeRunner = Script.runInContext('('+getSafeRunner.toString()+')()', context);
-//    var result;
-//    try {
-//        safeRunner({
-//            send: function (event, value) {
-//                "use strict";
-//                switch (event) {
-//                    case 'stdout':
-//                        consoleA.push(value); // DANB NOTE: this works (I don't know if it is safe)
-////            consoleA.push.apply(consoleA, JSON.parse(value).slice(1));
-//                        break;
-//                    case 'end':
-//                        result = JSON.parse(value)[0];
-//                        break;
-//                }
-//            }
-//        }, code);
-//    }
-//    catch (e) {
-//        result = "EXCEPTION: " + e.name + ': ' + e.message;
-//    }
-//    // @TODO: WAIT until all js has finished before being done
-//    consoleA.push("safeRunner returned");
-//    setTimeout( function() {
-//        consoleA.push("about to send the data back, and exit");
-//        process.stdout.on( 'drain', function() {
-//            process.exit(0)
-//        });
-//        process.stdout.write( JSON.stringify( { result: util.inspect( result ), console: consoleA } ) );
-//    }, 3000 );
-//
-//
-//}
-
