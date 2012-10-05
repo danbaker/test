c4919569696de2f8a1f1d0d4e785f5aa01430c65
Kip Lawrence <klawrence@corda.com>
2012-10-04 18:21:18 -0600

    Displays bots centered on the screen.

--- public/app/ContestView.js
+++ public/app/ContestView.js
@@ -17,7 +17,7 @@ define([
 
         render: function(data) {
 
-            console.log(data);
+            var self = this;
 
             if ( data.status == 404 ) {
 
@@ -33,18 +33,43 @@ define([
                 html.push( data.description );
                 html.push( '</p>' );
 
-                html.push( '<div id="editor" class="span8" style="height: 800px;">');
-                html.push( data.code || 'function yourcodehere() {\n\n}\n' );
-                html.push( '</div>');
+                var defaultCode = data.defaultCode;
 
-                html.push( '<div class="span11" style="padding-top:820px;"><button class="btn btn-primary pull-right" type="button">Save Code</button></div>' );
+                $.ajax({
+                    url: [ '/apis/1/contests', data._id, 'bots' ].join('/')
+                }).done(function(bots) {
 
-                this.$el.html( html.join('') );
-                this.$el.show();
+                        html.push( '<div class="btn-group" style="text-align:center;">' );
+                        _.each(bots, function(bot) {
+                            if ( bot.name ) {
+                                html.push( [ '<button class="btn">', bot.name, '</button>' ].join('') );
+                            }
+                        });
+                        html.push( '</div><br>' );
+                        html.push( '<div id="editor" class="span8" style="height: 800px;">');
+                        html.push( data.defaultCode || 'function yourcodehere() {\n\n}\n' );
+                        html.push( '</div>');
 
-                var editor = ace.edit("editor");
-                editor.setTheme("ace/theme/monokai");
-                editor.getSession().setMode("ace/mode/javascript");
+                        html.push( '<div class="span11" style="padding-top:820px;"><button class="btn btn-primary pull-right" type="button">Save Code</button></div>' );
+
+                        self.$el.html( html.join('') );
+                        self.$el.show();
+
+                        var editor = ace.edit("editor");
+                        editor.setTheme("ace/theme/monokai");
+                        editor.getSession().setMode("ace/mode/javascript");
+
+                    }).fail(function(data) {
+
+                        self.$el.html( html.join('') );
+                        self.$el.show();
+
+                        var editor = ace.edit("editor");
+                        editor.setTheme("ace/theme/monokai");
+                        editor.getSession().setMode("ace/mode/javascript");
+
+                    });
+
 
             }
             this.$el.fadeIn();
